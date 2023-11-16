adb71c8cec557518654972876724599f116e1ea4
kakzuha <86460341+kakzuha@users.noreply.github.com>
2023-11-16 14:33:30 +0700

    hapus_mhs.php

--- hapus_mhs.php
+++ hapus_mhs.php
@@ -3,5 +3,12 @@
 include 'koneksi.php';
 $nim = $_GET['nim'];
 $result = mysqli_query($koneksi,"DELETE FROM mahasiswa WHERE nim='$nim'");
+
+if ($result) {
+    echo "Record deleted successfully";
+    // Add redirection or further actions here if needed
+} else {
+    echo "Error deleting record: " . mysqli_error($koneksi);
+}
 // header("Location: mahasiswa.php");
 ?>
